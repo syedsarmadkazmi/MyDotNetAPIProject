b3c19b6c729a06bbbe7a4f9873bbf9712dc49ede
Sarmad Kazmi <sarmad.abbas@tentwenty.me>
2024-09-22 12:10:39 +0500

    chore: completed write data operations

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyDotNetAPIProject;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace MyApp.Namespace
 {
@@ -37,20 +38,63 @@ namespace MyApp.Namespace
 
         // POST api/<ProductsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
         {
+            if(!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            // an ActionResult provided by .NET similar to Ok and NotFound
+            return CreatedAtAction(
+                "GetSingleProduct", //get single product route
+                new {id = product.Id}, //params for get single product route
+                product // the object needs to be return in response
+            );
         }
 
         // PUT api/<ProductsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult> Put(int id, [FromBody] Product product)
         {
+            if( id != product.Id) {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if(!_context.Products.Any(product => product.Id == id)) {
+                    return NotFound();
+                }
+                else {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
         // DELETE api/<ProductsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult<Product>> Delete(int id)
         {
+            var product = await _context.Products.FindAsync(id);
+            if(product == null) {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product; 
         }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,13 +1,24 @@
-﻿namespace MyDotNetAPIProject;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyDotNetAPIProject;
 
 public class Product
 {
     public int Id {get; set;}
+
+    [Required]
     public string Sku {get; set;} = string.Empty;
+
+    [Required]
     public string Name {get; set;} = string.Empty;
+    [Required]
     public string Description {get; set;} = string.Empty;
+    [Required]
     public decimal Price {get; set;}
+    [Required]
     public bool IsAvailable {get; set;}
 
+    [Required]
     public int CategoryId {get; set;}
+    public virtual Category? Category {get; set;}
 }
